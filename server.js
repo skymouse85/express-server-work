a0090550ba2af19e5c9544db5d7f307c9394ee11
Mark Clifford <markmclifford@gmail.com>
2023-01-14 08:59:06 -0800

    express routers

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter')
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(express.json());
 
-app.use(express.static(__dirname + './public'));
+app.use('/campsites', campsiteRouter);
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text-html');
-    res.end(`<html><body><h1>This is an Express Server</h1></body/><html>`);
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-
-//creates an instance of the http server class, and start listening to it. 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
