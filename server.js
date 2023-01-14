f4e5b8b16dabf31229dec3c22cb6b2762a3ca5fc
Mark Clifford <markmclifford@gmail.com>
2023-01-13 22:16:29 -0800

    intro to express part 2

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + './public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-html');
+    res.end(`<html><body><h1>This is an Express Server</h1></body/><html>`);
+});
+
+
+//creates an instance of the http server class, and start listening to it. 
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
